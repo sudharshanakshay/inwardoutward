f513dbb927277c6f6a5ad1ec7e095b5acfa4aad2
sudharshan <sudharshan6acharya@gmail.com>
2022-04-28 12:55:22 +0530

    NavBar Updated

--- src/components/navBar/TopNavBar.js
+++ src/components/navBar/TopNavBar.js
@@ -1,9 +1,9 @@
-import { Navbar, Nav, NavDropdown, Container, Button } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Container, Button, Tabs, Tab, Form, FormControl } from "react-bootstrap";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { logOut } from "../../actions/auth/authSlice";
 import store from "../../store";
- 
+
 
 const TopNavBar = () => {
 
@@ -12,26 +12,41 @@ const TopNavBar = () => {
 
     return (
         <div>
-            <Navbar collapseOnSelect  expand="lg" >
-                <Container>
-                    <Navbar.Brand >
-                        <Link to='/dashboard'>Home</Link>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <Link to='/inward'>Inward Post</Link>
-                            <Link to='/outward'>Outward Post</Link>
-                            <Link to='/report'>Report</Link>
+            <Navbar bg="light" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand href="#">INWARDOUTWARD</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            {/* <Navigate to="/">Home</Navigate> */}
+                            <Nav.Link href="/inward">Inward</Nav.Link>
+                            <Nav.Link href="/outward">Outward</Nav.Link>
+                            <Nav.Link href="/report">Report</Nav.Link>
+                            {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
+                                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action5">
+                                    Something else here
+                                </NavDropdown.Item>
+                            </NavDropdown> */}
+                            {/* <Nav.Link href="#" disabled>
+                                Link
+                            </Nav.Link> */}
                         </Nav>
-                        <Nav>
-                            <Nav.Link eventKey={2} href="#memes">
-                                Logined as {user}
-                            </Nav.Link>
-                            <Nav>
-                                <button type="submit" onClick={()=>store.dispatch(logOut())}>logout</button>
-                            </Nav>
-                        </Nav>
+                        <Form className="d-flex">
+                            <FormControl
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
