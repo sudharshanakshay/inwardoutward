5ec924fc34c62c9cc15e9b467fe4683d45b85a62
sudharshan <sudharshan6acharya@gmail.com>
2022-05-02 21:09:24 +0530

    outward Page update

--- src/pages/Outward/outward.jsx
+++ src/pages/Outward/outward.jsx
@@ -1,14 +1,32 @@
 import React from "react";
-import Footer from "../../components/Footer/Footer";
 import TopNavBar from "../../components/navBar/TopNavBar";
+import { Container } from "react-bootstrap";
+import TableFair from "../../components/TableFair/TableFair";
+import { TABLEHEADER_LONG } from "../../utility/Values";
+import {getDisplayData} from '../../actions/posts/postsAction';
 
 const Outward = () => {
+
+    getDisplayData()
+
+    const OUTWARD_TABLE_TITLE = "Outward Table";
+
+    const tableDataString = sessionStorage.getItem('inwardTable');
+    const TABLE_DATA =  JSON.parse(tableDataString);
+
     return (
         <>
-            <TopNavBar/>
-            <div>outward page</div>
-            <Footer/>
+            <div >
+                <TopNavBar />
+            </div>
+            <div >
+                <Container className="data-table">
+                    <TableFair id="table" title={OUTWARD_TABLE_TITLE} tableHeaders={TABLEHEADER_LONG} tableRows={TABLE_DATA} applyDataTableApi={true} />
+                </Container>
+            </div>
         </>
     )
 }
+
+
 export default Outward;
