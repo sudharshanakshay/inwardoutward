407aafe67cd6fdd2babe282ed397df4ce66ec528
sudharshan <sudharshan6acharya@gmail.com>
2022-05-06 17:47:09 +0530

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>here comes Footer, copyright @ aptra</div>
+        <div>here comes Footer, copyright@Aptra</div>
     )
 }
 
